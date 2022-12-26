f2fdd9b16f551a663411b8d3e96c6b5c08df637c
Eniakz <eniakz@icloud.com>
2022-12-26 19:56:26 +0100

    Initial Commit

---
+++ CryptoNews/CryptoNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoNewsApp.swift
+//  CryptoNews
+//
+//  Created by EniaKz on 26/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
